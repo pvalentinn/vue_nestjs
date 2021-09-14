9696d3d17cfd617694c47557cc06b20a2db9bc22
LenGaL <pol.mansart@gmail.com>
2021-09-14 10:48:33 +0200

    [INIT] new nest project

---
+++ server/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27042/test')
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
