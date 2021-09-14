8a69cf74769ac8c579021bc2f83523c6413b6741
LenGaL <pol.mansart@gmail.com>
2021-09-14 13:39:10 +0200

    [INIT] vite vue starter

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello World" />
+</template>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+</style>
