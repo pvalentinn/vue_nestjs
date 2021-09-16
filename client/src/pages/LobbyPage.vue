f3e7cdd2d0490858a64c7d918837ac2b8c6dc882
LenGaL <pol.mansart@gmail.com>
2021-09-16 15:51:39 +0200

    [ADD] subscription updating players

--- client/src/pages/LobbyPage.vue
+++ client/src/pages/LobbyPage.vue
@@ -1,22 +1,25 @@
 <template>
     <div v-if="loading">Loading...</div>
-    <div v-else-if="!loading">
+    <div v-else-if="!loading && players">
         <h1>Welcome to lobby {{ id }}</h1>
         <h2>The currents players are :</h2>
         <ul id="example-1">
-            <li v-for="player in result.lobby.players" :key="player._id">{{ player.login }}</li>
+            <li v-for="player in players" :key="player._id">{{ player.login }}</li>
         </ul>
     </div>
 </template>
 
 <script setup lang='ts'>
-import { useQuery } from "@vue/apollo-composable";
-import gql from "graphql-tag";
+import { ref } from "vue";
+import { useQuery, useSubscription } from "@vue/apollo-composable";
 import { useRoute, useRouter } from "vue-router";
+import gql from "graphql-tag";
 
 let route = useRoute();
 let router = useRouter();
 let { id } = route.params;
+
+let players = ref<null | { _id: string, login: string }[]>(null);
 
 const GET_LOBBY = gql`
     query getLobby($id: String!){
@@ -30,14 +33,40 @@ const GET_LOBBY = gql`
     }
 `;
 
+const SUBSCRIPTION = gql`
+    subscription updatePlayers($id: String!) {
+        updatePlayers(id: $id) {
+            players {
+                _id,
+                login
+            }
+        }
+    }
+`;
+
 console.log(id);
 
-const { result, loading, error, onResult, onError } = useQuery(GET_LOBBY, { id });
+const { loading, onResult, onError } = useQuery(GET_LOBBY, { id });
+const { onResult: updatePlayers } = useSubscription(SUBSCRIPTION, { id });
+
+updatePlayers(result => {
+    console.log(result)
+    let updated_players = result.data.updatePlayers.players;
+    if (updated_players) {
+        console.log(updated_players);
+        players.value = updated_players;
+    }
+})
 
 onResult(res => {
-    console.log(result.value);
-    result.value
+    console.log(res.data)
+    if (res.data == undefined) {
+        router.push({ name: 'NotFound' });
+    } else {
+        players.value = res.data.lobby.players;
+    }
 });
+
 onError((e) => {
     console.log(e);
     router.push({ name: 'NotFound' });
